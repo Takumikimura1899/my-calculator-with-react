99844b76ff9d775f679fa26e5bd0d00b51b8b868
Takumikimura141899 <kimura141899@gmail.com>
2022-02-06 21:20:02 +0900

    [fix]0を押した時の処理が抜けていたため追加

--- src/logic/calculate.ts
+++ src/logic/calculate.ts
@@ -6,7 +6,7 @@ export interface State {
 }
 
 const isNumberButton = (button: string) => {
-  const NumArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const NumArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   return NumArray.includes(button);
 };
 
